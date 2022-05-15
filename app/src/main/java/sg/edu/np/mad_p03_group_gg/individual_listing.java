217aee090b9a27341aac88f33ee9666b2a5b7bd8
Lim Hong Ying <s10223298@connect.np.edu.sg>
2022-05-15 20:47:49 +0800

    Added 2 activities and layout files

---
+++ app/src/main/java/sg/edu/np/mad_p03_group_gg/individual_listing.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad_p03_group_gg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class individual_listing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_listing);
+    }
+}
