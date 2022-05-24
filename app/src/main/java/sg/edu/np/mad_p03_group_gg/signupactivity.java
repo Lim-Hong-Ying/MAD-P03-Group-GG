3524cfcfe41ad89b831e377af08c01d797b1bbee
Koh Han Jie Isaac <s10222275@connect.np.edu.sg>
2022-05-24 09:01:49 +0800

    Added Log-in Page and Sign-up page synced with Database

---
+++ app/src/main/java/sg/edu/np/mad_p03_group_gg/signupactivity.java
@@ -0,0 +1,94 @@
+package sg.edu.np.mad_p03_group_gg;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class signupactivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signupactivity);
+        //Get textview
+        TextView tologin= findViewById(R.id.Log_in_page);
+        //setonclick listener for tologin button
+
+
+        Button signup = findViewById(R.id.button);
+        auth=FirebaseAuth.getInstance();
+        //Check if users has registered
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register(v);
+
+            }
+
+        });
+
+
+        tologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Login = new Intent(signupactivity.this,
+                        loginpage.class);
+                startActivity(Login);//Starts sign up activity
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+    public void Register(View v){
+        EditText name = findViewById(R.id.setusername);
+        EditText Password = findViewById(R.id.enterpassword);
+        EditText Email = findViewById(R.id.emailaddr);
+        String email = Email.getText().toString().trim();
+        String password = Password.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            Email.setError("Email Required");
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Password.setError("Password required");
+            return;
+        }
+        if(password.length()<6){
+            Password.setError("Password lesser then 6 characters");
+            return;
+        }
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(signupactivity.this,"User created",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+
+                }
+            }
+        });
+
+    }
+}
