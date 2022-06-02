3497a8c46e9c258696f072080271beaa6bc52dfb
Lim Hong Ying <s10223298@connect.np.edu.sg>
2022-06-02 12:41:18 +0800

    Modified individualListingObject.java; Modified individual_listing.java; Partially implemented Firebase DB retrieval method

--- app/src/main/java/sg/edu/np/mad_p03_group_gg/individualListingObject.java
+++ app/src/main/java/sg/edu/np/mad_p03_group_gg/individualListingObject.java
@@ -1,24 +1,76 @@
 package sg.edu.np.mad_p03_group_gg;
 
-public class individualListingObject extends listingObject {
-    private String description;
-    private String location;
-    private Boolean delivery;
-    private String deliveryType;
-    private int deliveryPrice;
-    private int deliveryTime;
+public class individualListingObject {
+    private int pID; //listing ID
+    private String title;
+    private String sID; //seller ID
+    private String iC; //item condition
+    private int price; //item price
+    private String description; //item description
+    private String location; //deal location
+    private Boolean delivery; //is delivery available?
+    private String deliveryType; //if yes, what kind
+    private int deliveryPrice; //price for delivery
+    private int deliveryTime; //lead time for delivery
+    private Boolean reserved; //is item reserved?
 
     public individualListingObject() {
 
     }
 
-    public individualListingObject(String desc, String l, Boolean d, String dt, int dp, int dtime) {
+    public individualListingObject(int pid, String t, String sid, String cond, int p, String desc, String l, Boolean d, String dt, int dp, int dtime, Boolean r) {
+        pID = pid;
+        title = t;
+        sID = sid;
+        iC = cond;
+        price = p;
         description = desc;
         location = l;
         delivery = d;
         deliveryType = dt;
         deliveryPrice = dp;
         deliveryTime = dtime;
+        reserved = r;
+    }
+
+    public void setpID(int id) {
+        pID = id;
+    }
+
+    public int getpID() {
+        return pID;
+    }
+
+    public void setTitle(String t) {
+        title = t;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setsID(String id) {
+        sID = id;
+    }
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setiC(String c) {
+        iC = c;
+    }
+
+    public String getiC() {
+        return iC;
+    }
+
+    public void setPrice(int p) {
+        price = p;
+    }
+
+    public int getPrice() {
+        return price;
     }
 
     public void setDescription(String s) {
@@ -67,5 +119,13 @@ public class individualListingObject extends listingObject {
 
     public int getDeliveryTime() {
         return deliveryTime;
+    }
+
+    public void setReserved(Boolean r) {
+        reserved = r;
+    }
+
+    public Boolean getReserved() {
+        return reserved;
     }
 }
--- app/src/main/java/sg/edu/np/mad_p03_group_gg/individual_listing.java
+++ app/src/main/java/sg/edu/np/mad_p03_group_gg/individual_listing.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class individual_listing extends AppCompatActivity {
 
     @Override
@@ -29,5 +32,12 @@ public class individual_listing extends AppCompatActivity {
         deliveryoption = findViewById(R.id.individual_deliveryoption);
         deliveryprice = findViewById(R.id.individual_deliveryprice);
         deliverytime = findViewById(R.id.individual_deliverytime);
+    }
+
+    private void retrieveDataFromFB(int pid) {
+        String db = "https://cashoppe-179d4-default-rtdb.asia-southeast1.firebasedatabase.app/";
+        FirebaseDatabase individualdb = FirebaseDatabase.getInstance(db);
+        DatabaseReference individualListing = individualdb.getReference("individual-listing").child(""+pid);
+        individualListing.equalTo(pid);
     }
 }
