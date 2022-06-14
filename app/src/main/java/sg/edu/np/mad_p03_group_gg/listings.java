b547b53905925987ac0ba3f828fbf27fe3b0a3c6
Lim Hong Ying <s10223298@connect.np.edu.sg>
2022-06-14 23:00:43 +0800

    Implemented new view mode in listings.java;

--- app/src/main/java/sg/edu/np/mad_p03_group_gg/listings.java
+++ app/src/main/java/sg/edu/np/mad_p03_group_gg/listings.java
@@ -1,6 +1,5 @@
 package sg.edu.np.mad_p03_group_gg;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,7 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
-import android.widget.ToggleButton;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -43,11 +42,25 @@ public class listings extends AppCompatActivity {
             }
         });
 
-        ToggleButton viewMode = findViewById(R.id.view_mode);
+        Switch viewMode = findViewById(R.id.view_mode);
         viewMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                SharedPreferences sharedPreferences = getSharedPreferences("Cashopee", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
 
+                if (b == false) {
+                    editor.putString("view", "card");
+                    Log.e("VIEW CHANGED", "CARD");
+                }
+
+                else {
+                    editor.putString("view", "grid");
+                    Log.e("VIEW CHANGED", "GRID");
+                }
+
+                editor.commit();
+                recyclerViewStarter(data);
             }
         });
 
@@ -59,20 +72,23 @@ public class listings extends AppCompatActivity {
         RecyclerView listingRecycler = findViewById(R.id.listing_recycler);
         listing_adapter adapter = new listing_adapter(data);
 
-        LinearLayoutManager listingLayoutMgr = new LinearLayoutManager(this);
-        listingRecycler.setLayoutManager(listingLayoutMgr);
-        listingRecycler.setItemAnimator(new DefaultItemAnimator());
-        listingRecycler.setAdapter(adapter);
-    }
+        SharedPreferences sharedPreferences = getSharedPreferences("Cashopee", MODE_PRIVATE);
 
-    private void recyclerViewStarter2(ArrayList<listingObject> data) {
-        RecyclerView listingRecycler = findViewById(R.id.listing_recycler);
-        listing_adapter adapter = new listing_adapter(data);
+        String mode = sharedPreferences.getString("view", "");
 
-        GridLayoutManager listingLayoutMgr = new GridLayoutManager(this, 2);
-        listingRecycler.setLayoutManager(listingLayoutMgr);
-        listingRecycler.setItemAnimator(new DefaultItemAnimator());
-        listingRecycler.setAdapter(adapter);
+        if (mode == "card") {
+            LinearLayoutManager listingLayoutMgr = new LinearLayoutManager(this);
+            listingRecycler.setLayoutManager(listingLayoutMgr);
+            listingRecycler.setItemAnimator(new DefaultItemAnimator());
+            listingRecycler.setAdapter(adapter);
+        }
+
+        else {
+            GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+            listingRecycler.setLayoutManager(gridLayoutManager);
+            listingRecycler.setItemAnimator(new DefaultItemAnimator());
+            listingRecycler.setAdapter(adapter);
+        }
     }
 
     private ArrayList<listingObject> testlistings(ArrayList<listingObject> data) {
