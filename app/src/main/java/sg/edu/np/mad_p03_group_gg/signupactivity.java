a0be429c06184e1322f70a4ad09e8abc252959c2
Lim Hong Ying <s10223298@connect.np.edu.sg>
2022-06-14 23:31:40 +0800

    Readded dependencies

--- app/src/main/java/sg/edu/np/mad_p03_group_gg/signupactivity.java
+++ app/src/main/java/sg/edu/np/mad_p03_group_gg/signupactivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
