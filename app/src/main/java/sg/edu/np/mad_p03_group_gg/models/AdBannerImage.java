fe448193449cb192d88e28d07a3351a67d898e35
Kai Zhe Tan <s10219390@connect.np.edu.sg>
2022-06-15 19:24:31 +0800

    Implemented persistent app bar, affected activities have to be converted to fragments.

--- app/src/main/java/sg/edu/np/mad_p03_group_gg/models/AdBannerImage.java
+++ app/src/main/java/sg/edu/np/mad_p03_group_gg/models/AdBannerImage.java
@@ -1,6 +1,8 @@
 package sg.edu.np.mad_p03_group_gg.models;
 
 public class AdBannerImage {
+    // To populate the advertisement banners on the homepage, or MainActivity
+
     private int image;
 
     public AdBannerImage(int image) {
