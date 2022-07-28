37f1d38386d3461b806b406419d1a0f97d31519a
ryanojh <s10223647@connect.np.edu.sg>
2022-07-28 20:06:35 +0800

    Check and request for permissions added

--- app/src/main/java/sg/edu/np/mad_p03_group_gg/EventsPage.java
+++ app/src/main/java/sg/edu/np/mad_p03_group_gg/EventsPage.java
@@ -1,5 +1,6 @@
 package sg.edu.np.mad_p03_group_gg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -16,23 +17,36 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+
+import sg.edu.np.mad_p03_group_gg.view.ui.fragments.HomepageFragment;
 
 public class EventsPage extends AppCompatActivity {
     RecyclerView eventRV;
     EventsRecyclerViewAdapter eventsRecyclerViewAdapter;
     private static TextView eventTV;
     private ImageView backBtn;
+    private String name, location, time, date, desc, userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.events_page);
-        // Check and request for Calendar permissions
+        // Check and request for Calendar permissions if denied
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_CALENDAR)
                 != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
@@ -62,6 +76,12 @@ public class EventsPage extends AppCompatActivity {
     protected void onResume()
     {
         super.onResume();
+        // Initialise eventList
+        if (Event.eventsList.size() == 0) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            userId = user.getUid();
+            readFromFireBase(userId);
+        }
         initRecyclerView();
         // Count number of events created
         noOfEvent(Event.eventsList);
@@ -123,5 +143,57 @@ public class EventsPage extends AppCompatActivity {
     public void calendar(View view){
         Intent test = new Intent(EventsPage.this, WeekViewActivity.class);
         startActivity(test);
+    }
+
+    /**
+     * Read event details of user from Firebase
+     * @param userId
+     */
+    public void readFromFireBase(String userId){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://cashoppe-179d4-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference("Planner");
+        myRef.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    int eventId = Integer.parseInt(snapshot.getKey());
+                    name = snapshot.child("name").getValue(String.class);
+                    location = snapshot.child("location").getValue(String.class);
+                    time = snapshot.child("time").getValue(String.class);
+                    date = snapshot.child("date").getValue(String.class);
+                    LocalDate dt = LocalDate.parse(date, dtf);
+                    desc = snapshot.child("description").getValue(String.class);
+                    // Only display current events
+                    if (dt.isAfter(LocalDate.now()) || dt.isEqual(LocalDate.now())){
+                        Event event = new Event(eventId, name, location, dt, time, desc);
+                        Event.eventsList.add(event);
+                    }
+                    else {
+                        continue;
+                    }
+
+                    Collections.sort(Event.eventsList, new Comparator<Event>() {
+                        @Override
+                        public int compare(Event event1, Event event2) {
+                            if (event1.getDate() == event2.getDate()){
+                                Log.e("Same event Date", "Event date same");
+                                try {
+                                    return new SimpleDateFormat("hh:mm a").parse(event1.getTime()).compareTo(new SimpleDateFormat("hh:mm a").parse(event2.getTime()));
+                                } catch (ParseException e) {
+                                    return 0;
+                                }
+                            }
+                            return event1.getDate().compareTo(event2.getDate());
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("TAG", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
